7f463fbc92053fb08534d090b1db6b56a66b5039
xinHoboken <1992andygao@gmail.com>
2016-09-03 12:13:54 -0400

    tt

---
+++ testSQL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testSQL
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    string source = "Data Source=XIN;Initial Catalog=student;Integrated Security=True";
+        //    string select = "SELECT * FROM display.dbo.userInfo WHERE userName = "+"'"+"xin"+"'";
+        //    SqlConnection conn = new SqlConnection(source);
+        //    conn.Open();
+        //    SqlCommand cmd = new SqlCommand(select, conn);
+        //    SqlDataReader reader = cmd.ExecuteReader();
+        //    while (reader.Read())
+        //    {
+        //        //console.writeline("id : {0} name : {1}", reader[0], reader[1]);
+        //        string str = reader["userPassword"].ToString();
+        //        Console.WriteLine(str);
+        //    }
+        //    //Console.WriteLine(reader["name"].ToString());
+        //    conn.Close();
+        //    Console.Read();
+        //}
+    }
+}
---
+++ testSQL/test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testSQL
+{
+    class test
+    {
+        static void Main(string[] args)
+        {
+            string source = "Data Source=XIN;Initial Catalog=student;Integrated Security=True";
+            string insert = "insert student.dbo.tb2(name,age,address) values('licong',24,'RenMin University Beijing')";
+            SqlConnection conn = new SqlConnection(source);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(insert, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            Console.Read();
+        }
+    }
+}
